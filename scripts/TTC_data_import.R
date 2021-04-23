5000d26379365fd61a51b57f4ac74fed4a688bcd
levi24 <srivrac24@gmail.com>
2021-04-23 00:29:12 -0400

    Get raw data

--- scripts/TTC_data_import.R
+++ scripts/TTC_data_import.R
@@ -7,9 +7,12 @@
 install.packages("opendatatoronto")
 library(opendatatoronto)
 library(tidyverse)
+library(here)
 
-TTC_data <- 
-  opendatatoronto::search_packages("TTC Ridership Analysis") %>%
-  opendatatoronto::list_package_resources() %>%
-  filter(name == "1985-2019 Analysis of ridership") %>%
-  get_resource()
+###TTC_data <- 
+  ###opendatatoronto::search_packages("TTC Ridership Analysis") %>%
+  ###opendatatoronto::list_package_resources() %>%
+  ###filter(name == "1985-2019 Analysis of ridership") %>%
+  ###get_resource()
+
+TTC_data <- read_csv(here::here("inputs","data","1985-2019-analysis-of-ridership - 1985-2019 ACTUALS.csv"))
